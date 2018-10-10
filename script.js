2475b142ebaccb56387d14101d60c27eb25cd50f
Cassandra Matos <cassandra@matosmendez.com>
2018-10-10 08:18:06 -0700

    upload code base

---
+++ script.js
@@ -0,0 +1,42 @@
+class Employee {
+    constructor(name, salary, hireDate) {
+      this.name = name;
+      this.salary = salary;
+      this.hireDate = hireDate;
+    }
+      
+   greeting (){
+   console.log("Hello, my name is this.name + George.");{
+   
+   let George = newEmployee("George",8000,3/20/2017){
+   
+   George.greeting();
+   }
+   
+   class member extends users{
+   
+   get jobDescription (name, salary, hireDate, Manager)
+   super(name, salary, hireDate)
+   this.manager= job description{
+   
+   getDescription(){
+   console.log(this.manager +" is the jobDescription){
+   let George = new employee(George,8000,3/20/2017,jobDescription){
+   George.getjobDescription
+   
+   yearsExperience(name, salary, hireDate, Manager,Designer)
+   super(name, salary, hireDate)
+   this.designer = yearsExperience{
+   
+   getExperience(){
+   console.log(new employer +" has yearsExperience){
+   
+   degreeCompleted(name, salary, hireDate, Manager,Designer,SalesAssociate){
+   super(name, salary, hireDate)
+   this.SalesAssociate = degree{
+   
+   
+   degreeCompleted(){
+   console.log(new employer +" has a this.degreeCompleted in sales){
+   
+   }
